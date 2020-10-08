2f4ae02a7cca585205b0216f6387edc9c3f5511a
Blake <72426806+bostler3@users.noreply.github.com>
2020-10-07 19:29:55 -0600

    Blake saying "Hello"

--- src/prove04/Main.java
+++ src/prove04/Main.java
@@ -6,5 +6,6 @@ public class Main {
 
         System.out.println("Hello, my name is Nathan.");
         System.out.println("Hello Nathan, my name is Abby.");
+        System.out.println("Hello Nathan, my name is Blake.");
     }
 }
